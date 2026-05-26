76a05206b9ebaaac07d7c1a26452c0113f0936e0
suyash giri <suyashgiri938@gmail.com>
2026-05-26 20:30:00 +0530

    More programs added

---
+++ src/Strings/Contains.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Contains {
+    public static void main(String[] args) {
+        String s = "Jay ho";
+        System.out.println(s.contains("J"));
+
+    }
+}
---
+++ src/Strings/StartsWith.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class StartsWith {
+    public static void main(String[] args) {
+        String s = "RadheKrishna";
+        System.out.println(s.startsWith("Ra"));
+        System.out.println(s.endsWith("na"));
+
+    }
+}
