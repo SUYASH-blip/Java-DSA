a1669a95b502bc3da5f351ffb77085f254b71ce2
suyash giri <suyashgiri938@gmail.com>
2026-06-09 19:30:08 +0530

    Added

---
+++ src/StringBuilder/Basic_Methods.java
@@ -0,0 +1,9 @@
+package StringBuilder;
+
+public class Basic_Methods {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Java");
+        sb.setCharAt(2,'b');
+        System.out.println(sb);
+    }
+}
