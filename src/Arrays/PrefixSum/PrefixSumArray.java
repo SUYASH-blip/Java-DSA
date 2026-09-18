f06dc30543255fb15a5aba689426dae5dba581f4
suyash giri <suyashgiri938@gmail.com>
2026-09-18 19:33:25 +0530

    Codes Added For Previously Added Files

--- src/Arrays/PrefixSum/PrefixSumArray.java
+++ src/Arrays/PrefixSum/PrefixSumArray.java
@@ -1,4 +1,27 @@
 package Arrays.PrefixSum;
 
+import java.util.Arrays;
+
 public class PrefixSumArray {
+    public static int[] prefix_array_generator(int[] nums,int n){
+        int[] output = new int[n];
+        int left = 0;
+        for(int i = 0 ; i < n ; i ++){
+            if(i == 0){
+                left = 0;
+            }
+            else{
+                left += nums[i-1];
+            }
+            output[i] = left;
+        }
+        return output;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int n = nums.length;
+        int[] output = prefix_array_generator(nums,n);
+        System.out.println(Arrays.toString(output));
+    }
 }
--- src/Arrays/PrefixSum/SuffixSumArray.java
+++ src/Arrays/PrefixSum/SuffixSumArray.java
@@ -1,4 +1,28 @@
 package Arrays.PrefixSum;
 
+import java.util.Arrays;
+
 public class SuffixSumArray {
+    public static int[] suffix_array_generator(int[] nums,int n){
+        int[] output = new int[n];
+        int right = 0;
+        for(int i = n-1; i >= 0 ; i--){
+            if(i == n-1){
+                right = 0;
+            }
+            else{
+                right += nums[i+1];
+            }
+            output[i] = right;
+        }
+        return output;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int n = nums.length;
+        int[] output = suffix_array_generator(nums,n);
+        System.out.println(Arrays.toString(output));
+    }
 }
