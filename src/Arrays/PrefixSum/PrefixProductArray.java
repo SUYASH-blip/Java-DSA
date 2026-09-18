44830d8f81ade24d1393a74a0d1a3821267af7e3
suyash giri <suyashgiri938@gmail.com>
2026-09-18 19:38:30 +0530

    More Files Code Added

--- src/Arrays/PrefixSum/PrefixProductArray.java
+++ src/Arrays/PrefixSum/PrefixProductArray.java
@@ -1,4 +1,30 @@
 package Arrays.PrefixSum;
 
+import java.util.Arrays;
+
+import static Arrays.PrefixSum.SuffixSumArray.suffix_array_generator;
+
 public class PrefixProductArray {
+    public static int[] prefix_product_array_generator(int[] nums,int n){
+        int[] output = new int[n];
+        int left = 1;
+        for(int i = 0; i < n ; i++){
+            if(i == 0){
+                left = 1;
+            }
+            else{
+                left *= nums[i-1];
+            }
+            output[i] = left;
+        }
+        return output;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int n = nums.length;
+        int[] output = prefix_product_array_generator(nums,n);
+        System.out.println(Arrays.toString(output));
+    }
 }
